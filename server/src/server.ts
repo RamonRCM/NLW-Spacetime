944677ca4e57f9138466555109398d7874b1ee22
RamonRCM <costa.m.ramon@gmail.com>
2023-05-20 16:56:18 -0300

    back install cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true, // toda URL de front-end pode acessar meu back-end
+  // em produção, substutuir o 'true' para o endereço da aplicação
+})
 app.register(memoriesRoutes)
 
 app
